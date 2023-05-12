7416ea1e9a1117cdc85b8bcc8d6b497d5b917806
Luke <lukestevens@hotmail.com>
2023-05-12 13:48:32 +1000

    Framework v12
    plus whitespace changes

--- DateValidationActionTags.php
+++ DateValidationActionTags.php
@@ -10,66 +10,66 @@ use ExternalModules\AbstractExternalModule;
 
 class DateValidationActionTags extends AbstractExternalModule
 {
-        protected $is_survey = 0;
+    protected $is_survey = 0;
 
-        protected static $Tags = array(
-            '@FUTURE' => array('comparison'=>'gt', 'description'=>'Date Validation Action Tags<br>For a date (or datetime) field, validate that the value entered is <u>after today (or now)</u>.<br>Current date (time) is NOT within the allowed range.'),
-            '@NOTPAST' => array('comparison'=>'gte', 'description'=>'Date Validation Action Tags<br>For a date (or datetime) field, validate that the value entered is <u>today (or now) or after</u>.<br>Current date (time) IS within the allowed range.'),
-            '@PAST' => array('comparison'=>'lt', 'description'=>'Date Validation Action Tags<br>For a date (or datetime) field, validate that the value entered is <u>before today (or now)</u>.<br>Current date (time) is NOT within the allowed range.'),
-            '@NOTFUTURE' => array('comparison'=>'lte', 'description'=>'Date Validation Action Tags<br>For a date (or datetime) field, validate that the value entered is <u>today (or now) or before</u>.<br>Current date (time) IS within the allowed range.'),
-        );
-        
-        public function redcap_data_entry_form_top($project_id, $record=null, $instrument, $event_id, $group_id=null, $repeat_instance=1) {
-                $this->includeTagFunctions($instrument);
-        }
+    protected static $Tags = array(
+        '@FUTURE' => array('comparison'=>'gt', 'description'=>'Date Validation Action Tags<br>For a date (or datetime) field, validate that the value entered is <u>after today (or now)</u>.<br>Current date (time) is NOT within the allowed range.'),
+        '@NOTPAST' => array('comparison'=>'gte', 'description'=>'Date Validation Action Tags<br>For a date (or datetime) field, validate that the value entered is <u>today (or now) or after</u>.<br>Current date (time) IS within the allowed range.'),
+        '@PAST' => array('comparison'=>'lt', 'description'=>'Date Validation Action Tags<br>For a date (or datetime) field, validate that the value entered is <u>before today (or now)</u>.<br>Current date (time) is NOT within the allowed range.'),
+        '@NOTFUTURE' => array('comparison'=>'lte', 'description'=>'Date Validation Action Tags<br>For a date (or datetime) field, validate that the value entered is <u>today (or now) or before</u>.<br>Current date (time) IS within the allowed range.'),
+    );
+    
+    public function redcap_data_entry_form_top($project_id, $record, $instrument, $event_id, $group_id, $repeat_instance) {
+        $this->includeTagFunctions($instrument);
+    }
 
-        public function redcap_survey_page_top($project_id, $record=null, $instrument, $event_id, $group_id=null, $survey_hash=null, $response_id=null, $repeat_instance=1) {
-                $this->is_survey = 1;
-                $this->includeTagFunctions($instrument);
-        }
+    public function redcap_survey_page_top($project_id, $record, $instrument, $event_id, $group_id, $survey_hash, $response_id, $repeat_instance) {
+        $this->is_survey = 1;
+        $this->includeTagFunctions($instrument);
+    }
         
-        /**
-         * Augment the action_tag_explain content on project Design pages by 
-         * adding some additional tr following the last built-in action tag.
-         * @param type $project_id
-         */
-        public function redcap_every_page_before_render($project_id) {
-                if (PAGE==='Design/action_tag_explain.php') {
-                        global $lang;
-                        $lastActionTagDesc = end(\Form::getActionTags());
+    /**
+     * Augment the action_tag_explain content on project Design pages by 
+     * adding some additional tr following the last built-in action tag.
+     * @param type $project_id
+     */
+    public function redcap_every_page_before_render($project_id) {
+        if (PAGE==='Design/action_tag_explain.php') {
+            global $lang;
+            $lastActionTagDesc = end(\Form::getActionTags());
 
-                        // which $lang element is this?
-                        $langElement = array_search($lastActionTagDesc, $lang);
-                        
-                        foreach (static::$Tags as $tag => $tagAttr) {
-                                $lastActionTagDesc .= "</td></tr>";
-                                $lastActionTagDesc .= $this->makeTagTR($tag, $tagAttr['description']);
-                        }                        
-                        $lang[$langElement] = rtrim(rtrim(rtrim(trim($lastActionTagDesc), '</tr>')),'</td>');
-                }
+            // which $lang element is this?
+            $langElement = array_search($lastActionTagDesc, $lang);
+            
+            foreach (static::$Tags as $tag => $tagAttr) {
+                $lastActionTagDesc .= "</td></tr>";
+                $lastActionTagDesc .= $this->makeTagTR($tag, $tagAttr['description']);
+            }                        
+            $lang[$langElement] = rtrim(rtrim(rtrim(trim($lastActionTagDesc), '</tr>')),'</td>');
         }
+    }
+    
+    protected function includeTagFunctions($instrument) {
+        $taggedFields = array();
+        $tags = static::$Tags;
+        $instrumentFields = \REDCap::getDataDictionary('array', false, true, $instrument);
         
-        protected function includeTagFunctions($instrument) {
-                $taggedFields = array();
-                $tags = static::$Tags;
-                $instrumentFields = \REDCap::getDataDictionary('array', false, true, $instrument);
-                
-                // $pattern /(@FUTURE|@NOTPAST|@PAST|@NOTFUTURE)\s/
-                $pattern = '/('.implode('|', array_keys($tags)).')/';
-                foreach ($instrumentFields as $fieldName => $fieldDetails) {
-                        $matches = array();
-                        if (preg_match($pattern, $fieldDetails['field_annotation'], $matches)) {
-                                $comparison = $tags[$matches[0]]['comparison'];
-                                $taggedFields[$fieldName] = $comparison;
-                        }
-                }
-                if (count($taggedFields)>0) {
-                        $this->includeJS($taggedFields);
-                }
+        // $pattern /(@FUTURE|@NOTPAST|@PAST|@NOTFUTURE)\s/
+        $pattern = '/('.implode('|', array_keys($tags)).')/';
+        foreach ($instrumentFields as $fieldName => $fieldDetails) {
+            $matches = array();
+            if (preg_match($pattern, $fieldDetails['field_annotation'], $matches)) {
+                $comparison = $tags[$matches[0]]['comparison'];
+                $taggedFields[$fieldName] = $comparison;
+            }
         }
-        
-        protected function includeJS($taggedFields) {
-                ?>
+        if (count($taggedFields)>0) {
+            $this->includeJS($taggedFields);
+        }
+    }
+    
+    protected function includeJS($taggedFields) {
+        ?>
 <script type="text/javascript">
     $(document).ready(function(){
 
@@ -128,32 +128,31 @@ class DateValidationActionTags extends AbstractExternalModule
         });
     });
 </script>
-                <?php
-        }
-        
-        /**
-         * Make a table row for an action tag copied from 
-         * v8.5.0/Design/action_tag_explain.php
-         * @global type $isAjax
-         * @param type $tag
-         * @param type $description
-         * @return type
-         */
-        protected function makeTagTR($tag, $description) {
-                global $isAjax, $lang;
-                return \RCView::tr(array(),
-			\RCView::td(array('class'=>'nowrap', 'style'=>'text-align:center;background-color:#f5f5f5;color:#912B2B;padding:7px 15px 7px 12px;font-weight:bold;border:1px solid #ccc;border-bottom:0;border-right:0;'),
-				((!$isAjax || (isset($_POST['hideBtns']) && $_POST['hideBtns'] == '1')) ? '' :
-					\RCView::button(array('class'=>'btn btn-xs btn-rcred', 'style'=>'', 'onclick'=>"$('#field_annotation').val(trim('".js_escape($tag)." '+$('#field_annotation').val())); highlightTableRowOb($(this).parentsUntil('tr').parent(),2500);"), $lang['design_171'])
-				)
-			) .
-			\RCView::td(array('class'=>'nowrap', 'style'=>'background-color:#f5f5f5;color:#912B2B;padding:7px;font-weight:bold;border:1px solid #ccc;border-bottom:0;border-left:0;border-right:0;'),
-				$tag
-			) .
-			\RCView::td(array('style'=>'font-size:12px;background-color:#f5f5f5;padding:7px;border:1px solid #ccc;border-bottom:0;border-left:0;'),
-				'<i class="fas fa-cube mr-1"></i>'.$description
-			)
-		);
-
-        }
+            <?php
+    }
+    
+    /**
+     * Make a table row for an action tag copied from 
+     * v8.5.0/Design/action_tag_explain.php
+     * @global type $isAjax
+     * @param type $tag
+     * @param type $description
+     * @return type
+     */
+    protected function makeTagTR($tag, $description) {
+        global $isAjax, $lang;
+        return \RCView::tr(array(),
+            \RCView::td(array('class'=>'nowrap', 'style'=>'text-align:center;background-color:#f5f5f5;color:#912B2B;padding:7px 15px 7px 12px;font-weight:bold;border:1px solid #ccc;border-bottom:0;border-right:0;'),
+                ((!$isAjax || (isset($_POST['hideBtns']) && $_POST['hideBtns'] == '1')) ? '' :
+                    \RCView::button(array('class'=>'btn btn-xs btn-rcred', 'style'=>'', 'onclick'=>"$('#field_annotation').val(trim('".js_escape($tag)." '+$('#field_annotation').val())); highlightTableRowOb($(this).parentsUntil('tr').parent(),2500);"), $lang['design_171'])
+                )
+            ) .
+            \RCView::td(array('class'=>'nowrap', 'style'=>'background-color:#f5f5f5;color:#912B2B;padding:7px;font-weight:bold;border:1px solid #ccc;border-bottom:0;border-left:0;border-right:0;'),
+                $tag
+            ) .
+            \RCView::td(array('style'=>'font-size:12px;background-color:#f5f5f5;padding:7px;border:1px solid #ccc;border-bottom:0;border-left:0;'),
+                '<i class="fas fa-cube mr-1"></i>'.$description
+            )
+        );
+    }
 }
